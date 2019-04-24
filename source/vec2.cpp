9d03ceddaf5376ed6b8c9e71d1710a5315b30f08
Irene Lopez Garcia <lopezg.irene@gmail.com>
2019-04-24 17:28:56 +0200

    Add Vec2 definition

---
+++ source/vec2.cpp
@@ -0,0 +1,25 @@
+#include "vec2.hpp"
+
+Vec2& Vec2::operator+=(Vec2 const& v) {
+  x += v.x;
+  y += v.y;
+  return *this;
+}
+
+Vec2& Vec2::operator-=(Vec2 const& v) {
+  x -= v.x;
+  y -= v.y;
+  return *this;
+}
+
+Vec2& Vec2::operator*=(float s) {
+  x *= s;
+  y *= s;
+  return *this;
+}
+
+Vec2& Vec2::operator/=(float s) {
+  x *= s;
+  y /= s;
+  return *this;
+}
